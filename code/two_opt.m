255eeb6cfbe338f31476c61dffaa02ce2156c715
RubenPants <broekxruben@gmail.com>
2019-12-12 18:39:43 +0100

    Update two_opt.m

--- code/two_opt.m
+++ code/two_opt.m
@@ -1,34 +1,29 @@
 %{
 two_opt.m
 
-Swap two cities 
+Swap two cities and check if the local distance decreased. If so, return
+the mutated chromosome back.
 
 Reference paper: https://arxiv.org/ftp/arxiv/papers/1409/1409.3078.pdf
 %}
 
-% Based on 
-%Take for each 4 sequential cities the two middle cities, swap them and check
-%whether this configuration is better than the previous one
-
-%Chrom= the matrix containing all the individuals
-%Dist = The matrix containing the distances between the cities
-%NVar = the number of cities
-%NInd = the number of individuals
-function best_chrom =  two_opt(Chrom, Dist)
-    best_chrom = Chrom;
-    best_dist = tspfun(Chrom, Dist, 2);
-    
-    for i=1:size(Chrom,2)-1
-        NewChrom = Chrom(1,:);
-        
-        % Do the swap
-        a = NewChrom(i); NewChrom(i) = NewChrom(i+1); NewChrom(i+1) = a;
-        
-        % Evaluate
-        new_dist = tspfun(NewChrom, Dist, 2);
-        if new_dist < best_dist
-            best_dist = new_dist;
-            best_chrom = NewChrom;
+function Chrom =  two_opt(Chrom, Dist)
+    for i=1:size(Chrom,2)-3
+        if local_distance(swap(Chrom(1,i:i+3)), Dist) < local_distance(Chrom(1,i:i+3), Dist)
+            Chrom(1,i:i+3) = swap(Chrom(1,i:i+3));
+            break;
         end
+    end
+end
+
+% Swap the second and third element from a four-valued chromosome
+function Chrom = swap(Chrom)
+    a = Chrom(2); Chrom(2) = Chrom(3); Chrom(3) = a;
+end
+
+function dist = local_distance(Chrom, Dist)
+    dist = Dist(Chrom(1), Chrom(size(Chrom,2)));
+    for i=1:size(Chrom,2)-1
+        dist = dist + Dist(Chrom(i), Chrom(i+1));
     end
 end
