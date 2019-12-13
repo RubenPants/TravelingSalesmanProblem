2fce156dfe98ad4e9cb25f07922d833ee1a54e9e
RubenPants <broekxruben@gmail.com>
2019-12-13 12:05:30 +0100

    Added exercise4 figures and updated code

--- code/exercises/exercise4/exercise4.m
+++ code/exercises/exercise4/exercise4.m
@@ -13,6 +13,16 @@ Performance scores depict how much longer the found solution is compared to
 the real optimum.
 %}
 
+% Parameters
+global GENERATIONS RUNS CALCULATE_NEW
+CALCULATE_NEW = false;
+GENERATIONS = 100;
+RUNS = 20;
+
+% Add paths to other files
+addpath 'C:\Users\Ruben\Documents\Projects\TravelingSalesmanProblem\code'
+addpath 'C:\Users\Ruben\Documents\Projects\TravelingSalesmanProblem\code\datasets'
+
 % Load all the datasets
 data_list = ["016", "018", "023", "025", "048", "050", "051", "067", "070", "100", "127"];
 optima = load('rondrit_optima.tsp');
@@ -20,23 +30,57 @@ optima = load('rondrit_optima.tsp');
 HEURISTIC = ["off", "inversion", "2-opt", "both"];
 
 % Run the experiment
-for h=HEURISTIC
-    fprintf("Calculating %s - Progress: ", h)
-    total = zeros(1,length(data_list));
-    for i=1:length(data_list)
-        intermediate = zeros(1, 10);
-        for j=1:10
-            output = run_data(data_list(i), h);
-            intermediate(j) = output("minimum");
+if CALCULATE_NEW
+    best_f = zeros(length(data_list), length(HEURISTIC), GENERATIONS);
+    mean_f = zeros(length(data_list), length(HEURISTIC), GENERATIONS);
+    worst_f = zeros(length(data_list), length(HEURISTIC), GENERATIONS);
+    for h=1:length(HEURISTIC)
+        fprintf("Calculating %s - Progress: ", HEURISTIC(h))
+        total = zeros(1,length(data_list));
+        for i=1:length(data_list)
+            intermediate = zeros(1, RUNS);
+            intermediate_best = zeros(1,GENERATIONS);
+            intermediate_mean = zeros(1,GENERATIONS);
+            intermediate_worst = zeros(1,GENERATIONS);
+            for j=1:RUNS
+                output = run_data(data_list(i), HEURISTIC(h));
+                intermediate(j) = output("minimum");
+                intermediate_best = intermediate_best + output("best");
+                intermediate_mean = intermediate_mean + output("mean_fits");
+                intermediate_worst = intermediate_worst + output("worst");
+            end
+            total(i) = geomean(intermediate) / optima(i);
+            best_f(i,h,:) = intermediate_best / length(data_list);
+            mean_f(i,h,:) = intermediate_mean / length(data_list);
+            worst_f(i,h,:) = intermediate_worst / length(data_list);
+            fprintf("#")
         end
-        total(i) = geomean(intermediate) / optima(i);
-        fprintf("#")
+        performance = mean(total) * 100;  % Percentage of 100 is perfect match
+        fprintf("\nResult: %s - performance: %.2f\n\n", HEURISTIC(h), performance);
     end
-    performance = mean(total) * 100;  % Percentage of 100 is perfect match
-    fprintf("\nResult: %s - performance: %.2f\n\n", h, performance);
+    % Save all the matrices
+    save('total', 'total');
+    save('best_f', 'best_f');
+    save('mean_f', 'mean_f');
+    save('worst_f', 'worst_f');
 end
 
+% Load in the matrices
+file = matfile('total'); total = file.total;
+file = matfile('best_f'); best_f = file.best_f;
+file = matfile('mean_f'); mean_f = file.mean_f;
+file = matfile('worst_f'); worst_f = file.worst_f;
+
+% Display all the figures
+for i=1:length(data_list)
+    title = sprintf("%s.fig", data_list(i));
+    create_figure(data_list(i), best_f(i,:,:), mean_f(i,:,:), worst_f(i,:,:));
+end
+
+% Function called to run each of the configurations
 function c = run_data(set, h)
+    global GENERATIONS
+    
     % Load data
     data = load(sprintf('rondrit%s.tsp', set));
     x=data(:,1)/max([data(:,1);data(:,2)]);y=data(:,2)/max([data(:,1);data(:,2)]);
@@ -45,7 +89,7 @@ function c = run_data(set, h)
     data = containers.Map;
     data("x") = x;
     data("y") = y;
-    data("maxgen") = 100;
+    data("maxgen") = GENERATIONS;
     data("representation") = "path";
     data("crossover") = "heuristic_crossover";
     data("pr_cross") = 0.2;
@@ -58,11 +102,49 @@ function c = run_data(set, h)
     c = run_ga(data);
 end
 
+% Function called to visualize the progress
+function create_figure(title, best_f, mean_f, worst_f)
+    figure();
+    x=0:length(best_f)-1;
+    
+    % Split up data
+    best_off = squeeze(best_f(1,1,:));
+    %best_inversion = squeeze(best_f(1,2,:));
+    %best_2_opt = squeeze(best_f(1,3,:));
+    best_both = squeeze(best_f(1,4,:));
+    
+    mean_off = squeeze(mean_f(1,1,:));
+    %mean_inversion = squeeze(mean_f(1,2,:));
+    %mean_2_opt = squeeze(mean_f(1,3,:));
+    mean_both = squeeze(mean_f(1,4,:));
+    
+    worst_off = squeeze(worst_f(1,1,:));
+    %worst_inversion = squeeze(worst_f(1,2,:));
+    %worst_2_opt = squeeze(worst_f(1,3,:));
+    worst_both = squeeze(worst_f(1,4,:));
+    
+    hold on
+    %plot(x, best_off, 'g', x, best_inversion,'g--', x, best_2_opt, 'g:', x, best_both, 'g-.')
+    %plot(x, mean_off, 'b', x, mean_inversion,'b--', x, mean_2_opt, 'b:', x, mean_both, 'b-.')
+    %plot(x, worst_off, 'r', x, worst_inversion,'r--', x, worst_2_opt, 'r:', x, worst_both, 'r-.')
+    plot(x, best_off, 'g', x, best_both, 'g--')
+    plot(x, mean_off, 'b', x, mean_both, 'b--')
+    plot(x, worst_off, 'r', x, worst_both, 'r--')
+    ylabel("Fitness")
+    xlabel("Generation")
+    %legend("best off", "best inversion", "best 2-opt", "best both", "mean off", "mean inversion", "mean 2-opt", "mean both", "worst off", "worst inversion", "worst 2-opt", "worst both");
+    legend("best off", "best both", "mean off", "mean both", "worst off", "worst both");
+    savefig(title);
+    hold off
+end
+
+
+
 %{
 Results:
 
-Result: off - performance: 104.02
-Result: inversion - performance: 103.61
-Result: 2-opt - performance: 102.42
-Result: both - performance: 102.32
+Result: off - performance: 104.30
+Result: inversion - performance: 103.67
+Result: 2-opt - performance: 102.70
+Result: both - performance: 102.20
 %}
--- code/exercises/exercise4/test.m
+++ code/exercises/exercise4/test.m
@@ -1,15 +1,9 @@
+x = linspace(0,pi);
+y1 = cos(x);
+plot(x,y1)
 
+hold on 
+y2 = cos(2*x);
+plot(x,y2)
 
-% Add paths to other files
-% addpath 'D:\User\Documents\School\Genetics\TravelingSalesmanProblem\code'
-% addpath 'D:\User\Documents\School\Genetics\TravelingSalesmanProblem\code\datasets'
-
-data = load(['rondrit016.tsp']);
-x=data(:,1)/max([data(:,1);data(:,2)]);
-y=data(:,2)/max([data(:,1);data(:,2)]);
-data = containers.Map;
-data("x") = x;
-data("y") = y;
-data("local_heur_pr")=0.3;
-data("local_heur")='2-opt';
-result = run_ga(data);
+legend('cos(x)','cos(2x)')
